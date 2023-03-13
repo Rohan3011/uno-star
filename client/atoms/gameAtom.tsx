ee21638c19d85d3705187165a518fb0887a9a5b1
rohan3011 <rohankamble3011@gmail.com>
2023-03-13 12:54:03 +0530

    Added card deck state

--- client/atoms/gameAtom.tsx
+++ client/atoms/gameAtom.tsx
@@ -17,17 +17,28 @@ export const turnState = atom<PlayerType>({
   default: undefined,
 });
 
-export const player1DeckState = atom({
+export const player1DeckState = atom<CardType[]>({
   key: "Player1Deck",
   default: [],
 });
 
-export const player2DeckState = atom({
+export const player2DeckState = atom<CardType[]>({
   key: "Player2Deck",
   default: [],
 });
 
+export const cardDeckState = atom<CardType[]>({
+  key: "CardDeck",
+  default: undefined,
+});
+
+// Card on top of deck
 export const activeCardState = atom<CardType>({
   key: "ActiveCard",
+  default: undefined,
+});
+
+export const playedCardState = atom<CardType>({
+  key: "PlayedCard",
   default: undefined,
 });
